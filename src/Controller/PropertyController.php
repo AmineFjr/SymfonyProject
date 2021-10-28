5593680df9d113a39c94808a6bef94d0dedf0f18
AmineFjr <amine.f@dalvin.fr>
2021-10-28 11:01:48 +0200

    add data to view

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,28 +13,27 @@ class PropertyController extends AbstractController
 {
 
     /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    /**
+     * @var ObjectManager
+     */
+    private $em;
+
+    /*public function __construct(PropertyRepository $repository, ObjectManager $em)
+    {
+
+        $this->repository = $repository;
+        $this->em = $em;
+    }*/
+
+    /**
      * @Route("/biens",name="property.index")
      * @return Response
      */
     public function index():Response
     {
-        $property = new Property();
-        $property->setTitle('Mon premier bien')
-            ->setPrice(200000)
-            ->setRooms(4)
-            ->setBedrooms(3)
-            ->setDescription('Une ptit description')
-            ->setSurface(60)
-            ->setFloor(4)
-            ->setHeat(1)
-            ->setCity('Montpellier')
-            ->setAdress('15 boulevard Gambetta')
-            ->setPostalCode('34000');
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($property);
-        $em->flush();
-
         return $this->render('property/index.html.twig',['current_menu' => 'properties']);
     }
 }
--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Property;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +18,34 @@ class PropertyRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Property::class);
+    }
+
+    /**
+     * @return Property[]
+     */
+    public function findAllVisible():array
+    {
+        return $this->findVsibleQuery()
+            ->where('p.sold = false')
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return Property[]
+     */
+    public function findLatest():array
+    {
+        return $this->findVsibleQuery()
+            ->where('p.sold = false')
+            ->setMaxResults(4)
+            ->getQuery()
+            ->getResult();
+    }
+    private function findVsibleQuery(): QueryBuilder
+    {
+        return $this->createQueryBuilder('p')
+            ->where('p.sold = false');
     }
 
     // /**
@@ -47,4 +76,5 @@ class PropertyRepository extends ServiceEntityRepository
         ;
     }
     */
+
 }
