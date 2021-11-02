0dda1aced2be669d3fb2824be5cce8365ffea939
AmineFjr <amine.f@dalvin.fr>
2021-11-02 15:30:13 +0100

    add options

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Persistence\ObjectManager;
 use Knp\Component\Pager\PaginatorInterface;
@@ -38,12 +40,14 @@ class PropertyController extends AbstractController
     public function index(PropertyRepository $repository,PaginatorInterface $paginator, Request $request):Response
     {
 
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class,$search);
+        $form->handleRequest($request);
 
 
+        $properties = $paginator->paginate($repository->findAllVisibleQuery($search),$request->query->getInt('page',1),12);
 
-        $properties = $paginator->paginate($repository->findAllVisibleQuery(),$request->query->getInt('page',1),12);
-
-        return $this->render('property/index.html.twig',['current_menu' => 'properties', 'properties' => $properties]);
+        return $this->render('property/index.html.twig',['current_menu' => 'properties', 'properties' => $properties, 'form' => $form->createView()]);
 
 
     }
